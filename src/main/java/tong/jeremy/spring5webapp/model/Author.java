442dffb898d3a6d0f83cff9d257103be4803a551
Tong <jeremy.tong@capgemini.com>
2019-05-06 16:04:49 -0400

    spelling error

--- src/main/java/tong/jeremy/spring5webapp/model/Author.java
+++ src/main/java/tong/jeremy/spring5webapp/model/Author.java
@@ -4,16 +4,19 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-
-
-
+/**
+ * Created by jt on 5/16/17.
+ */
 @Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books  = new HashSet<>();
 
     public Author() {
--- src/main/java/tong/jeremy/spring5webapp/model/Book.java
+++ src/main/java/tong/jeremy/spring5webapp/model/Book.java
@@ -9,12 +9,16 @@ import java.util.Set;
  */
 @Entity
 public class Book {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
